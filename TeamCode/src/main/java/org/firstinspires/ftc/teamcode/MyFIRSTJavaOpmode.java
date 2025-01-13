415e00dd53a43a9c29e1c4f559fa7ac0fcf64e62
Mattiemateo <130217368+Mattiemateo@users.noreply.github.com>
2025-01-13 10:00:59 +0100

    fix for the claw and arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpmode.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Blinker;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
